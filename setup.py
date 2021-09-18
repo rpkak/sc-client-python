f56970b279c4bda69495b87f2b6e1fd99595499f
rpkak <rpkak@users.noreply.github.com>
2021-09-18 22:53:45 +0200

    finisch version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_readme():
 
 setup(
     name='sc-client',
-    version='1.2.2-alpha',
+    version='1.2.2',
     packages=find_packages(),
     install_requires=get_requirements(),
     entry_points={
