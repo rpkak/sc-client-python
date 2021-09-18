77e39a7e74e306cdd9cf8d14a0e7fc0e21fed288
rpkak <rpkak@users.noreply.github.com>
2021-09-18 22:35:17 +0200

    Version 1.3.0 Alpha

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_readme():
 
 setup(
     name='sc-client',
-    version='1.2.1',
+    version='1.3.0-alpha',
     packages=find_packages(),
     install_requires=get_requirements(),
     entry_points={
